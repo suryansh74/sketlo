8f0dea18eb5ad14215dbdce3422ffbaefccc069a
suryansh74 <suryanshawasthi56@gmail.com>
2026-02-01 00:18:24 +0530

    feat: implement chi router with BDD test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/suryansh74/sketlo
 
 go 1.24.9
+
+require github.com/go-chi/chi/v5 v5.2.4 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
-	println("Hello  world")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", SayHello)
+	http.ListenAndServe(":8000", r)
+}
+
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("hello world"))
 }
